d1ea2ca9ab190bb2fbbc14565cf8233794fb7331
PsychoShaman <111572177+PsychoShaman@users.noreply.github.com>
2023-02-24 12:33:55 +0100

    finish layout class

--- data.mjs
+++ data.mjs
@@ -1,2 +1,44 @@
 //file to store all crucial data 
 
+
+let passionsArray = 
+[
+    {
+        id: 1,
+        title: "Programming",
+        description: "programming description",
+        svg: "./src/programming-svgrepo-com.svg",
+        images: [
+          "https://images.unsplash.com/photo-1676444263524-b12650053e83?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1073&q=80",
+        ],
+      },
+      {
+        id: 2,
+        title: "Painting",
+        description: "coding description",
+        svg: "./src/painting-svgrepo-com.svg",
+        images: [
+          "https://images.unsplash.com/photo-1674574124345-02c525664b65?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        ],
+      },
+      {
+        id: 3,
+        title: "Playing Djembe",
+        description: "music description",
+        svg: "./src/djembe-svgrepo-com.svg",
+        images: [
+          "https://images.unsplash.com/photo-1676694090990-b9e29828fdd3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1112&q=80",
+        ],
+      },
+      {
+        id: 4,
+        title: "Literature",
+        description: "cooking description",
+        svg: "./src/culture-svgrepo-com.svg",
+        images: [
+          "https://images.unsplash.com/photo-1674574124475-16dd78234342?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        ],
+      },
+];
+
+export {passionsArray};
--- index.mjs
+++ index.mjs
@@ -1,5 +1,6 @@
 // contains functions that create layout to the main page
-
+//import files
+import { passionsArray } from "./data.mjs";
 
 class setLayout
 {
@@ -8,6 +9,8 @@ class setLayout
         //setting up functions
         this.setMainContainer();
         this.setNavBar();
+        this.setAbout();
+        this.setPassions();
     }
 
     //main container
@@ -19,6 +22,30 @@ class setLayout
         navBar.classList.add("navbar");
         navBar.id = "navbar";   
         mainContainer.appendChild(navBar);
+        //about section
+        let about = document.createElement("section");
+        about.classList.add("about");
+        about.id = "about";
+        about.textContent = "About";
+        mainContainer.appendChild(about);
+        //passions items
+        let passions = document.createElement("section");
+        passions.classList.add("passions");
+        passions.id = "passions";
+        mainContainer.appendChild(passions);
+        //selected-passion item
+        let selectedPassion = document.createElement("section");
+        selectedPassion.classList.add("selected-passion");
+        selectedPassion.id = "selected-passion";
+        mainContainer.appendChild(selectedPassion);
+        //footer
+        let footer = document.createElement("footer");
+        footer.classList.add("footer");
+        footer.id = "footer";
+        footer.textContent = "Footer";
+        
+        mainContainer.appendChild(footer);
+        
     }
 
     setNavBar()
@@ -29,15 +56,73 @@ class setLayout
         title.classList.add("navbar-element");
         title.id = "title";
         title.textContent = "Marcin Dończyk";
-
-
+        //links 
+        let navLinks = document.createElement("ul");
+        navLinks.classList.add("navbar-links");
+        //home
+        let home = document.createElement("li");
+        home.classList.add("navbar-link");
+        home.id = "home";
+        home.innerHTML = "<a href='#'>Home</a>";
+        navLinks.appendChild(home);
+        //gallery
+        let gallery = document.createElement("li");
+        gallery.classList.add("navbar-link");
+        gallery.id = "gallery";
+        gallery.innerHTML = "<a href='#'>Gallery</a>";
+        navLinks.appendChild(gallery);
+        //form
+        let form = document.createElement("li");
+        form.classList.add("navbar-link");
+        form.id = "form";
+        form.innerHTML = "<a href='#'>Form</a>";
+        navLinks.appendChild(form);
+    
         //append elements
         navBar.appendChild(title);
+        navBar.appendChild(navLinks);
+    }
 
-        
+    setAbout()
+    {
+        let about = document.getElementById("about");
+        about.textContent = "About";
 
     }
     
+    setPassions()
+    {
+        let passions = document.getElementById("passions");
+        let passionItems = document.createElement('ul');
+        passionItems.classList.add('passion-items');
+        passionItems.id = "passion-items";
+
+
+        //items from array
+        passionsArray.forEach(
+            (item) => {
+                let li = document.createElement('li');
+                li.id = item.id;
+                //icon
+                let icon = document.createElement('img');
+                icon.src = item.svg;
+                icon.id = item.id;
+                icon.classList.add('passion-icon');
+                li.append(icon);
+                
+                //title
+                let title = document.createElement('h2');
+                title.textContent = item.title;
+                li.appendChild(title);
+                let description = document.createElement('p');
+                description.textContent = item.description;
+                li.appendChild(description);
+                passionItems.appendChild(li);
+            }
+        )
+        passions.appendChild(passionItems);
+
+    }
 }
 
 export default setLayout;
